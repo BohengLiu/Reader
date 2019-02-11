8f453610c3719c1f02b9f7281e045c4441e2b333
liuboheng <bohengliu@163.com>
2019-02-11 09:14:21 +0800

    update

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,27 @@
 import React,{Component } from 'react'
 
 export class Login extends Component{
+  constructor(){
+    super()
+    this.state = {
+      file:''
+    }
+  }
+  logfile(e){
+    e.persist()
+    console.log(e)
+  }
+
+  fileonload(){
+    console.log()
+  }
+
   render() {
     return (
-      <div>login</div>
+      <div>
+        <h1>Login</h1>
+        <input id="file" type='file' onChange={this.logfile.bind(this)} />
+      </div>
     )
   }
 }
