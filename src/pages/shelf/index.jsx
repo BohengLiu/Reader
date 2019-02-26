ced1c1253a2cac017d55e022e6a7f0e003b99f49
刘博恒 <lbh9311@sina.com>
2019-02-26 23:43:57 +0800

    finish input

--- src/pages/shelf/index.jsx
+++ src/pages/shelf/index.jsx
@@ -1,7 +1,13 @@
 import React,{Component } from 'react'
-import {Book} from 'epubjs'
+import ePub from 'epubjs'
 
 export class Shelf extends Component{
+  
+  constructor() {
+    super()
+    this._book = ePub()
+    this.rendition = {}
+  }
   
   logfile(e) {
     e.persist()
@@ -9,37 +15,26 @@ export class Shelf extends Component{
     let targetFile = e.target.files[0]
     console.log(file)
     if (window.FileReader) {
-      var reader = new FileReader();
-      reader.readAsArrayBuffer(targetFile);
-      console.log(reader)
+      const reader = new FileReader();
+      
+      // console.log(reader)
       reader.onload = (e) => {
-        console.log('onload e:',e)
-        let book = new Book({
-          bookPath: e.target.result
-        })
-        console.log(book)
-        
-          // var key = new Date().getTime() + '',
-          //     name = metadata.bookTitle,
-          //     author = metadata.creator,
-          //     article = new Book(key, name, author, e.target.result);
+        // console.log('onload e:',e)
+        // let book = new ePub({
+        //   bookPath: e.target.result
+        // })
+        const bookData = e.target.result
+        this._book.open(bookData)
 
-          // console.log(metadata);
-          // addBookToPage(article);
-          // bookDB.open(function () {
-          //     bookDB.addBook(
-          //         article,
-          //         function () {
-          //             console.log('add book successfully!');
-          //         },
-          //         function () {
-          //             console.log('some error occured!');
-          //         }
-          //     );
-          // });
-      // });
+        console.log(this._book)
+        this.rendition = this._book.renderTo("viewer", {
+          width: "100%",
+          height: 600
+        });
 
+        this.rendition.display();
       }
+      reader.readAsArrayBuffer(targetFile);
     } else {
       alert('Your browser does not support the required features. Please use a modern browser such as Google Chrome, or Mozilla Firefox')
     }
@@ -54,6 +49,11 @@ export class Shelf extends Component{
       <div>
         <h1>Book Shelf</h1>
         <input id="file" type='file' onChange={this.logfile.bind(this)} />
+        <div id="title"><input type="file" id="input" /></div>
+          <div id="viewer" className="spreads"></div>
+          <a id="prev" href="#prev" className="arrow">‹</a>
+          <a id="next" href="#next" className="arrow">›</a>
+
       </div>
     )
   }
